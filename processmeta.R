2c288d3c000423f45ccf7e6a95a6391c61312998
Arthur Gilly <arthur.gilly@helmholtz-muenchen.de>
2021-05-21 09:53:03 +0700

    update processmeta add ln pn

--- processmeta.R
+++ processmeta.R
@@ -100,127 +100,132 @@ names(anc.distr)=c("AFR", "AMR", "EAS", "EUR", "SAS")
 print(anc.distr)
 
 ### MAIN
-cat(paste("Reading target additive dataset\n"))
-flush.console()
-target=fread(paste0(indir, "/",infile,".target.add.traw"))
-ttfam=fread(paste0(indir, "/",infile,".target.tfam"))
+for(suffix in c("pn", "ln")){
+  if(suffix=="pn"){cat(paste("Pointnormal model\n"))}else{cat(paste("Lognormal model\n"))}
+  cat(paste("Reading target additive dataset\n"))
+  flush.console()
+  target=fread(paste0(indir, "/",infile,".target.add.",suffix,".traw"))
+  ttfam=fread(paste0(indir, "/",infile,".target.",suffix,".tfam"))
 
-toselect=c(2,7:ncol(target))
-target=target[,..toselect]
-setnames(target, "SNP", "rs")
-setnames(target, colnames(target), sub("_.*", "", colnames(target)))
-
-
-cat(paste("Reading ancestry-specific results\n"))
-flush.console()
-mvmeta=fread(paste0(indir,"/mvmeta.out"))
-pca=fread(paste0(indir,"/pca.txt"))
+  toselect=c(2,7:ncol(target))
+  target=target[,..toselect]
+  setnames(target, "SNP", "rs")
+  setnames(target, colnames(target), sub("_.*", "", colnames(target)))
 
 
-
-cat(paste("Building ancestry-specific scores\n"))
-flush.console()
-tst=as.data.table(t(apply(mvmeta, 1, function(x){
-    snp=x["SNP"]
-    x=as.numeric(x[-1])
-    c(snp, t(x[4]+pca$V1*x[6]+pca$V2*x[9]+pca$V3*x[12]))})))
-pops=c("GWD", "LWK", "MSL", "YRI", "CLM", "MXL", "PEL", "PUR", "CDX", "CHB", "JPT", "KHV", "FIN", "GBR", "IBS", "TSI", "BEB", "GIH", "PJL", "STU")
-setnames(tst, c("SNP", pops))
+  cat(paste("Reading ancestry-specific results\n"))
+  flush.console()
+  mvmeta=fread(paste0(indir,"/mvmeta.",suffix,".out"))
+  pca=fread(paste0(indir,"/pca.txt"))
 
 
-cat(paste("Reading target association sumstats\n"))
-flush.console()
-popdat=list()
-for(p in unique(nmtbl$V1)){
-    if(file.exists(paste0(indir,"/",p,".target.qassoc"))){
-            popdat[[p]]=fread(paste0(indir,"/",p,".target.qassoc"),select=c("SNP","BETA" ,"SE"))
-    }
-}
 
-resul=NULL
+  cat(paste("Building ancestry-specific scores\n"))
+  flush.console()
+  tst=as.data.table(t(apply(mvmeta, 1, function(x){
+      snp=x["SNP"]
+      x=as.numeric(x[-1])
+      c(snp, t(x[4]+pca$V1*x[6]+pca$V2*x[9]+pca$V3*x[12]))})))
+  pops=c("GWD", "LWK", "MSL", "YRI", "CLM", "MXL", "PEL", "PUR", "CDX", "CHB", "JPT", "KHV", "FIN", "GBR", "IBS", "TSI", "BEB", "GIH", "PJL", "STU")
+  setnames(tst, c("SNP", pops))
 
-## Ancestry specific stuff first
 
-for(p in unique(nmtbl$V1)){
-    ### indirect
-    angroup=nmtbl$V2[nmtbl$V1==p]
-    tosimulate=anc.distr[angroup]*n.samples
-    POPPERSUP=4
-    perpop=tosimulate/POPPERSUP
-    popcol=colnames(target)[grep(p, colnames(target))]
-    if(!length(popcol)){next}
-    cat(paste("\t\t Ancestry-specific, indirect method", p,"\n"))
-    flush.console()
-    totest=merge(tst[,c("SNP", p),with=F], popdat[[p]], by="SNP")
-    totest=merge(totest, mvmeta[,c("SNP", "V2"), with=F])
-    setnames(totest, c("SNP", p, "BETA", "SE", "V2"), c("SNP","mvbeta", "beta", "se","P"))
-    toconvert=c("mvbeta", "beta", "se","P")
-    totest[, (toconvert) := lapply(.SD, as.numeric), .SDcols = toconvert]
-    totest=totest[P<P_THRESHOLD,]
-    gtxs=grs.summary(totest$mvbeta, totest$beta, totest$se, perpop)
-    add=data.table(PRS=p, target=p, nsnp=nrow(totest), method="indirect", cor=gtxs$ahat[1], Standard.Error=gtxs$aSE[1], P=gtxs$pval[1], varex=gtxs$R2m[1])
-    resul=rbindlist(list(resul, add), use.names=T)
-    ## direct
-    cat(paste("\t\t Ancestry-specific, direct method", p,"\n"))
-    flush.console()
-    m=merge(tst[,c("SNP", p),with=F], target, by.x="SNP", by.y="rs")
-    m=merge(m, mvmeta[,c("SNP", "V2"), with=F],by="SNP")
-    setnames(m, "V2", "P")
-    m=m[P<P_THRESHOLD,]
-    toconvert=p
-    m[, (toconvert) := lapply(.SD, as.numeric), .SDcols = toconvert]
-    prs=t(t(lapply(m[,..popcol], function(x){sum(m[[p]]*x)})))
-    prs=data.table(id=rownames(prs), prs=unlist(prs))
-    prs=merge(prs, ttfam, by.x="id", by.y="V1")
-    add=cor.test.plus(prs$prs, prs$V6)
-    add$PRS=p
-    add$target=p
-    add$nsnp=nrow(m)
-    add$varex=add$cor*add$cor
-    add$method="direct"
-    resul=rbindlist(list(resul, add), use.names=T)
-}
+  cat(paste("Reading target association sumstats\n"))
+  flush.console()
+  popdat=list()
+  for(p in unique(nmtbl$V1)){
+      if(file.exists(paste0(indir,"/",p,".target.",suffix,".qassoc"))){
+              popdat[[p]]=fread(paste0(indir,"/",p,".target.",suffix,".qassoc"),select=c("SNP","BETA" ,"SE"))
+      }
+  }
 
-for (angroup in c("AFR", "AMR", "EAS", "EUR", "SAS", "TE")){
-    cat(paste("Processing score from ", angroup,"\n"))
-    flush.console()
-    ancmeta=fread(paste0(indir, "/metasoft.",angroup,".out"),
-    select=c("RSID","PVALUE_FE" ,"BETA_FE"))
-    ancmeta=ancmeta[PVALUE_FE<P_THRESHOLD,c("RSID", "BETA_FE")]
-    cat(paste("Number of SNPs in PRS:",nrow(ancmeta), "\n"))
-    for(p in unique(nmtbl$V1)){
-        ### Exact method corr with phenotype
-        cat(paste("\t Using target population", p,"\n"))
-        cat(paste("\t\t Direct method", p,"\n"))
-        flush.console()
-        m=merge(ancmeta, target, by.x="RSID", by.y="rs")
-        popcol=colnames(target)[grep(p, colnames(target))]
-        if(!length(popcol)){next}
-        prs=t(m[,lapply(.SD, function(x){sum(BETA_FE*x)}), .SDcols=popcol])
-        prs=data.table(id=rownames(prs), prs=prs)
-        prs=merge(prs, ttfam, by.x="id", by.y="V1")
-        add=cor.test.plus(prs$prs.V1, prs$V6)
-        add$PRS=angroup
-        add$target=p
-        add$nsnp=nrow(ancmeta) 
-        add$method="direct"
-        add$varex=add$cor*add$cor
-        if(is.null(resul)){resul=add}else{resul=rbind(resul,add)}
+  resul=NULL
 
-        ## approx method
-        cat(paste("\t\t Indirect method", p,"\n"))
-        flush.console()
-        if(angroup=="TE"){perpop=n.samples}else{
-          tosimulate=anc.distr[angroup]*n.samples
-          POPPERSUP=4
-          perpop=tosimulate/POPPERSUP
-        }
+  ## Ancestry specific stuff first
 
-        ancmeta.pop=merge(ancmeta, popdat[[p]], all.x=T, by.x="RSID", by.y="SNP", suffixes = c("", paste0(".", p)))
-        gtxs=grs.summary(ancmeta.pop$BETA_FE, ancmeta.pop$BETA, ancmeta.pop$SE, perpop)
-        add=data.table(PRS=angroup, target=p, nsnp=nrow(ancmeta.pop), method="indirect", cor=gtxs$ahat[1], Standard.Error=gtxs$aSE[1], P=gtxs$pval[1], varex=gtxs$R2m[1])
-        resul=rbindlist(list(resul, add), use.names=T)
+  for(p in unique(nmtbl$V1)){
+      ### indirect
+      angroup=nmtbl$V2[nmtbl$V1==p]
+      tosimulate=anc.distr[angroup]*n.samples
+      POPPERSUP=4
+      perpop=tosimulate/POPPERSUP
+      popcol=colnames(target)[grep(p, colnames(target))]
+      if(!length(popcol)){next}
+      cat(paste("\t\t Ancestry-specific, indirect method", p,"\n"))
+      flush.console()
+      totest=merge(tst[,c("SNP", p),with=F], popdat[[p]], by="SNP")
+      totest=merge(totest, mvmeta[,c("SNP", "V2"), with=F])
+      setnames(totest, c("SNP", p, "BETA", "SE", "V2"), c("SNP","mvbeta", "beta", "se","P"))
+      toconvert=c("mvbeta", "beta", "se","P")
+      totest[, (toconvert) := lapply(.SD, as.numeric), .SDcols = toconvert]
+      totest=totest[P<P_THRESHOLD,]
+      gtxs=grs.summary(totest$mvbeta, totest$beta, totest$se, perpop)
+      add=data.table(model=suffix, PRS=p, target=p, nsnp=nrow(totest), method="indirect", cor=gtxs$ahat[1], Standard.Error=gtxs$aSE[1], P=gtxs$pval[1], varex=gtxs$R2m[1])
+      resul=rbindlist(list(resul, add), use.names=T)
+      ## direct
+      cat(paste("\t\t Ancestry-specific, direct method", p,"\n"))
+      flush.console()
+      m=merge(tst[,c("SNP", p),with=F], target, by.x="SNP", by.y="rs")
+      m=merge(m, mvmeta[,c("SNP", "V2"), with=F],by="SNP")
+      setnames(m, "V2", "P")
+      m=m[P<P_THRESHOLD,]
+      toconvert=p
+      m[, (toconvert) := lapply(.SD, as.numeric), .SDcols = toconvert]
+      prs=t(t(lapply(m[,..popcol], function(x){sum(m[[p]]*x)})))
+      prs=data.table(id=rownames(prs), prs=unlist(prs))
+      prs=merge(prs, ttfam, by.x="id", by.y="V1")
+      add=cor.test.plus(prs$prs, prs$V6)
+      add$model=suffix
+      add$PRS=p
+      add$target=p
+      add$nsnp=nrow(m)
+      add$varex=add$cor*add$cor
+      add$method="direct"
+      resul=rbindlist(list(resul, add), use.names=T)
+  }
 
-    }
+  for (angroup in c("AFR", "AMR", "EAS", "EUR", "SAS", "TE")){
+      cat(paste("Processing score from ", angroup,"\n"))
+      flush.console()
+      ancmeta=fread(paste0(indir, "/metasoft.",angroup,".",suffix,".out"),
+      select=c("RSID","PVALUE_FE" ,"BETA_FE"))
+      ancmeta=ancmeta[PVALUE_FE<P_THRESHOLD,c("RSID", "BETA_FE")]
+      cat(paste("Number of SNPs in PRS:",nrow(ancmeta), "\n"))
+      for(p in unique(nmtbl$V1)){
+          ### Exact method corr with phenotype
+          cat(paste("\t Using target population", p,"\n"))
+          cat(paste("\t\t Direct method", p,"\n"))
+          flush.console()
+          m=merge(ancmeta, target, by.x="RSID", by.y="rs")
+          popcol=colnames(target)[grep(p, colnames(target))]
+          if(!length(popcol)){next}
+          prs=t(m[,lapply(.SD, function(x){sum(BETA_FE*x)}), .SDcols=popcol])
+          prs=data.table(id=rownames(prs), prs=prs)
+          prs=merge(prs, ttfam, by.x="id", by.y="V1")
+          add=cor.test.plus(prs$prs.V1, prs$V6)
+          add$model=suffix
+          add$PRS=angroup
+          add$target=p
+          add$nsnp=nrow(ancmeta)
+          add$method="direct"
+          add$varex=add$cor*add$cor
+          if(is.null(resul)){resul=add}else{resul=rbindlist(list(resul,add), use.names=T)}
+
+          ## approx method
+          cat(paste("\t\t Indirect method", p,"\n"))
+          flush.console()
+          if(angroup=="TE"){perpop=n.samples}else{
+            tosimulate=anc.distr[angroup]*n.samples
+            POPPERSUP=4
+            perpop=tosimulate/POPPERSUP
+          }
+
+          ancmeta.pop=merge(ancmeta, popdat[[p]], all.x=T, by.x="RSID", by.y="SNP", suffixes = c("", paste0(".", p)))
+          gtxs=grs.summary(ancmeta.pop$BETA_FE, ancmeta.pop$BETA, ancmeta.pop$SE, perpop)
+          add=data.table(model=suffix, PRS=angroup, target=p, nsnp=nrow(ancmeta.pop), method="indirect", cor=gtxs$ahat[1], Standard.Error=gtxs$aSE[1], P=gtxs$pval[1], varex=gtxs$R2m[1])
+          resul=rbindlist(list(resul, add), use.names=T)
+
+      }
+  }
 }
 fwrite(resul, paste(opt[["out"]], "prs.fit.txt", sep="."), sep="\t", quote=F)
