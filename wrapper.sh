c5829ac885cf1eb4696b96617a38646335014f65
Arthur Gilly <agilly@users.noreply.github.com>
2021-05-21 09:28:44 +0700

    1st change ln pn

--- wrapper.sh
+++ wrapper.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-## TODO add the lognormal model
 ## TODO add the Wu model
 
 nsnp=$1
@@ -28,20 +27,29 @@ fi
 
 
 for pop in GWD LWK MSL YRI CLM MXL PEL PUR CDX CHB JPT KHV FIN GBR IBS TSI BEB GIH PJL STU; do
-  awk '{if(!($1~/'$pop'/)){$NF=-9}}1' 'OFS=\t' $baseout.tfam > $pop.tfam;
-  plink --tped $baseout.tped --tfam $pop.tfam --allow-no-sex --assoc --out $pop
-  awk '{if(!($1~/'$pop'/)){$NF=-9}}1' 'OFS=\t' $baseout.target.tfam > $pop.target.tfam
-  plink --tped $baseout.target.tped --tfam $pop.target.tfam --allow-no-sex --assoc --out $pop.target
+  awk '{if(!($1~/'$pop'/)){$NF=-9}}1' 'OFS=\t' $baseout.pn.tfam > $pop.pn.tfam;
+  plink --tped $baseout.tped --tfam $pop.pn.tfam --allow-no-sex --assoc --out $pop.pn
+  awk '{if(!($1~/'$pop'/)){$NF=-9}}1' 'OFS=\t' $baseout.target.pn.tfam > $pop.target.pn.tfam
+  plink --tped $baseout.target.tped --tfam $pop.target.pn.tfam --allow-no-sex --assoc --out $pop.target.pn
+
+  awk '{if(!($1~/'$pop'/)){$NF=-9}}1' 'OFS=\t' $baseout.ln.tfam > $pop.ln.tfam;
+  plink --tped $baseout.tped --tfam $pop.ln.tfam --allow-no-sex --assoc --out $pop.ln
+  awk '{if(!($1~/'$pop'/)){$NF=-9}}1' 'OFS=\t' $baseout.target.ln.tfam > $pop.target.ln.tfam
+  plink --tped $baseout.target.tped --tfam $pop.target.ln.tfam --allow-no-sex --assoc --out $pop.target.ln
+
 done
 
 
 ./makemeta.R $(pwd) .target
 for f in `ls metasoft.*.txt`; do cp $f $f.bak; awk 'NR>1' $f | sponge $f; done
 
-for p in AFR AMR EAS EUR SAS TE; do java -jar /Metasoft.jar -input metasoft.$p.txt -pvalue_table /HanEskinPvalueTable.txt -output metasoft.$p.out; done
+for p in AFR AMR EAS EUR SAS TE; do java -jar /Metasoft.jar -input metasoft.$p.pn.txt -pvalue_table /HanEskinPvalueTable.txt -output metasoft.$p.pn.out; done
+for p in AFR AMR EAS EUR SAS TE; do java -jar /Metasoft.jar -input metasoft.$p.ln.txt -pvalue_table /HanEskinPvalueTable.txt -output metasoft.$p.ln.out; done
+
 ./mvmeta.R
 
-plink --tfile $baseout.target --recode A-transpose --out $baseout.target.add
+plink --tfile $baseout.target.pn --recode A-transpose --out $baseout.target.add.pn
+plink --tfile $baseout.target.ln --recode A-transpose --out $baseout.target.add.ln
 
 #sadness, the metasoft.out files have garbage columns at the end (and spelling mistakes in col names :((( )
 for f in `ls metasoft.*.out`; do cut -f1-18 $f| sponge $f; done
